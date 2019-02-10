b9695fcfab4e4069674791393b68cb605ea62313
Rego <pvrego@users.noreply.github.com>
2019-02-10 17:01:58 -0200

    Move main server application to task model.

--- src/comm-sockets.ads
+++ src/comm-sockets.ads
@@ -1,5 +1,18 @@
+with GNAT.Sockets;
+
 -- =============================================================================
 -- Implements the sockets layers for the client/server application
 -- =============================================================================
 package COMM.SOCKETS is
+
+   package SOCKETS renames GNAT.Sockets;
+
+   --  Multicast group: administratively scoped IP address
+   Group : constant String := "239.255.128.128";
+
+   task Socket_Manager is
+      entry Start;
+      entry Stop;
+   end Socket_Manager;
+
 end COMM.SOCKETS;
