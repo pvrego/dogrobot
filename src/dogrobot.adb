9740f172c8d573b8dfe53fc0edc047266e314e77
Rego <pvrego@users.noreply.github.com>
2019-02-13 19:23:56 -0200

    Re-enable init devices

--- src/dogrobot.adb
+++ src/dogrobot.adb
@@ -32,9 +32,8 @@ begin
    end;
 
 #elsif Module = "MOD_RPI" then
-   --     DEVS.SYSFS.Init_Devices;
-   DEVS.SYSFS.DeInit_Devices;
-
+   DEVS.SYSFS.Init_Devices;
+   --     DEVS.SYSFS.DeInit_Devices;
 #end if;
 
    COMM.SOCKETS.Socket_Manager.Start;
